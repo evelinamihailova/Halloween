bdc656704945be6a19e5f48044c0b9ba19da5935
evelinamihailova <evelinamihailova@gmail.com>
2016-10-11 23:31:25 +0200

    css updates
    added background, fixed lens, added some fonts, adjusted layout

--- halloween_release/js/app.js
+++ halloween_release/js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation();
+var changeBg = function () {
+  if ($(".bg").hasClass("night")) {
+    $(".bg").removeClass("night");
+    $(".bg-switch").removeClass("switched");
+  } else {
+    $(".bg").addClass("night");
+    $(".bg-switch").addClass("switched");
+  }
+};
