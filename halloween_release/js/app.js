e9277a04cbc0f35e5462f30720f505b9db7c6def
evelinamihailova <evelinamihailova@gmail.com>
2016-10-14 00:13:59 +0200

    small changes

--- halloween_release/js/app.js
+++ halloween_release/js/app.js
@@ -1,9 +1,28 @@
-var changeBg = function () {
-  if ($(".bg").hasClass("night")) {
-    $(".bg").removeClass("night");
-    $(".bg-switch").removeClass("switched");
-  } else {
-    $(".bg").addClass("night");
-    $(".bg-switch").addClass("switched");
+var mood = '';
+var bg = $('.bg');
+var moodSwitch = $('#moodSwitch');
+
+var setMood = function (mood) {
+  switch(mood) {
+    case 'evil':
+        bg.attr('class', 'bg evil');
+      break;
+    case 'happy':
+      
+        bg.attr('class', 'bg happy');
+      break;
+    case 'naughty':
+      
+        bg.attr('class', 'bg naughty');
+      break;
+    default: bg.attr('class', 'bg depressed');
   }
 };
+      
+$(document).ready(function() {
+  setMood();
+});
+$(moodSwitch).click(function(e) {
+  debugger;
+  setMood();
+});
