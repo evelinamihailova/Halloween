3e4125ed8e40107350bf6a64cad92004fad8cfc6
Evelina Mihailova <evelinamihailova@gmail.com>
2016-10-14 16:09:30 +0200

    adding some functionality and changing svgs

--- halloween_release/js/app.js
+++ halloween_release/js/app.js
@@ -1,28 +1,52 @@
-var mood = '';
 var bg = $('.bg');
 var moodSwitch = $('#moodSwitch');
-
+var depressedAudio = new Audio('../sounds/Hl.mp3');
+var happyAudio = new Audio('../sounds/Maniacal Witches Laugh.mp3');
+var evilAudio = new Audio('../sounds/Demon_Your_Soul_is_mine.mp3');
+var customAudio = new Audio('../sounds/Pain.mp3');
+var hideFaces = function(){
+  $('#customFace').hide();
+  $('#happyFace').hide();
+  $('#evilFace').hide();
+  $('#depressedFace').hide();
+};
 var setMood = function (mood) {
-  switch(mood) {
-    case 'evil':
-        bg.attr('class', 'bg evil');
-      break;
-    case 'happy':
-      
-        bg.attr('class', 'bg happy');
-      break;
-    case 'naughty':
-      
-        bg.attr('class', 'bg naughty');
-      break;
-    default: bg.attr('class', 'bg depressed');
+  switchPumpkin(mood);
+  bg.attr('class', 'bg' + ' ' + mood);
+  moodSwitch.attr('class', 'multistate-switch' + ' ' + mood);
+};
+
+var switchPumpkin = function (mood) {
+  hideFaces();
+  $('#' + mood + 'Face').show();
+  switch(mood){
+    case "depressed":
+      depressedAudio.play();
+    break;
+    case "evil":
+      evilAudio.play();
+    break;
+    case "happy":
+      happyAudio.play();
+    break;
+    case "custom":
+      customAudio.play();
+    break;
   }
 };
-      
-$(document).ready(function() {
-  setMood();
+moodSwitch.click(function(e) {
+  var mood, elm = $(e.target);
+
+  if(elm.is("li") ){
+    mood = elm.data("mood");
+  } else if (elm.is("span")){
+    mood = elm.parentElement.data("mood");
+  }
+  if(mood) setMood(mood);
 });
-$(moodSwitch).click(function(e) {
-  debugger;
-  setMood();
+
+$(document).ready(function() {
+  //show popup with question door
+  
+  setMood('depressed');
 });
