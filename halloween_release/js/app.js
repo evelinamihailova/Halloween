306bb48ed72a9552b7de7b18131bd42890b208e2
Evelina Mihailova <evelinamihailova@gmail.com>
2016-10-17 18:08:29 +0200

    added more faces, fixed some functionality

--- halloween_release/js/app.js
+++ halloween_release/js/app.js
@@ -1,7 +1,7 @@
 $(document).foundation();
 
 var bg = $('.bg');
-var depressedAudio = new Audio('../sounds/Hl.mp3');
+var sadAudio = new Audio('../sounds/Hl.mp3');
 var happyAudio = new Audio('../sounds/Maniacal Witches Laugh.mp3');
 var evilAudio = new Audio('../sounds/Demon_Your_Soul_is_mine.mp3');
 var customAudio = new Audio('../sounds/Pain.mp3');
@@ -9,34 +9,74 @@ var hideFaces = function(){
   $('#customFace').hide();
   $('#happyFace').hide();
   $('#evilFace').hide();
-  $('#depressedFace').hide();
+  $('#sadFace').hide();
 };
-
+var audio = false;
 
 $(document).ready(function() { 
   $('#trickOrTreat').foundation('open');
   
-  $('#showTrick').click(function(){
-    setMood('evil');
-  });
-  $('#showTreat').click(function(){
-    setMood('happy');
-  });
+  $('#showTrick').click(function(){ setMood('evil'); });
+  $('#showTreat').click(function(){ setMood('happy'); });
+  $('#showCustom').click(function(){ setMood('custom'); });
   
   $('#moodSwitch').click(function(e) {
-  var mood, elm = $(e.target);
+    var mood, elm = $(e.target);
 
-  if(elm.is("li") ){
-    mood = elm.data("mood");
-  } else if (elm.is("span")){
-    mood = elm.parent().data("mood");
-  }
-  if(mood) setMood(mood);
-});
+    if(elm.is('li') ){
+      mood = elm.data('mood');
+    } else if (elm.is('span')){
+      mood = elm.parent().data('mood');
+    }
+    if(mood) setMood(mood);
+  });
   
-  var matrices = $('#ledMatrics').data('matrices');
-  ledMatrics(matrices);
+  var matrix = $('#ledMatrix').data('matrix');
+  ledMatrix(matrix);
+  
+  $('#colorPicker').submit(function(e) {
+    e.preventDefault();
+    var color = $(this).find('input[type="color"]').val();
+//    $.ajax({
+//           type: "POST",
+//           url: url,
+//           data: $("#idForm").serialize(), // serializes the form's elements.
+//           success: function(data){}
+//         });
+
+  });
+  $('.lens-container svg').click(function() {
+    //take picture
+    
+  });
+  
+  toScrollOrNotToScroll($('#ledMatrix p'));
+  
+  $('#ledMatrix p').click(function() {
+    var text = $(this).html();
+    $(this).replaceWith('<input value="'+text+'" type=text>');
+    console.log(this);
+    $(this).focus();
+  });
+
+  $('#ledMatrix input').keypress(function(e) {
+    var
+    var text = $(this).val();
+    if(e.which == 13) {
+      $(this).blur();
+      $(this).replaceWith('<p>'+text+'</p>');
+      toScrollOrNotToScroll('#ledMatrix p');
+    }
+  });
 });
+
+function toScrollOrNotToScroll(elm) {    
+  if($(elm).html().length > 3){
+    $(elm).addClass('scrolling');
+  } else {
+    $(elm).removeClass('scrolling');
+  }
+}
 
 function setMood(mood) {
   $('.main').removeClass('hide');
@@ -48,26 +88,28 @@ function setMood(mood) {
 function switchPumpkin(mood) {
   hideFaces();
   $('#' + mood + 'Face').show();
-  switch(mood){
-    case "depressed":
-     // depressedAudio.play();
-    break;
-    case "evil":
-     // evilAudio.play();
-    break;
-    case "happy":
-     // happyAudio.play();
-    break;
-    case "custom":
-     // customAudio.play();
-    break;
+  if(audio){
+    switch(mood){
+      case "sad":
+        sadAudio.play();
+      break;
+      case "evil":
+        evilAudio.play();
+      break;
+      case "happy":
+        happyAudio.play();
+      break;
+      case "custom":
+        customAudio.play();
+      break;
+    }
   }
 }
 
 // MATRIX
 
-function ledMatrics(amount) {
-  var matrices = amount;
+function ledMatrix(amount) {
+  var matrix = amount;
   var rows = 7;
   var columns = 7;
   var html='', lines='';
@@ -79,9 +121,9 @@ function ledMatrics(amount) {
     lines += '</li>';
   }
   var frame = '<ul>' + lines + '</ul>';
-  for(i=0;i<matrices;i++){
+  for(i=0;i<matrix;i++){
     html+=frame;
   }
-  html+="<div class='text'><p>Boo</p></div>";
-  $('#ledMatrics').html(html); 
+  html+='<div class="text"><p>Booo</p>';
+  $('#ledMatrix').html(html); 
 }
