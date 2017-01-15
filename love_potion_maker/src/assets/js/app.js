8e7c1e830c6765ad719583b968fd5b7be4c2cd9b
evelinamihailova <evelinamihailova@gmail.com>
2017-01-16 00:19:35 +0100

    adding colours

--- love_potion_maker/src/assets/js/app.js
+++ love_potion_maker/src/assets/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation();
+var bluePotion = $('img.blue'),
+    greenPotion = $('img.green'),
+    yellowPotion = $('img.blue'),
+    orangePotion = $('img.blue'),
+    redPotion = $('img.blue'),
+    purplePotion = $('img.blue');
+
+$(bluePotion).click(function(){
+  
+});
+
+//getState("blue").save({"data": "2"});
+//getState("temperature","Report").get("data");
+//var cam = getValue("camera");
+//cam.save({':type': 'urn:seluxit:xml:bastard:value-1.1', ':id': cam.get(':id'), status: "update"},{
+//  patch: true,
+//  success: function(){
+//  
+//  }
+//});
